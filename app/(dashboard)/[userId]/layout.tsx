7db789c25091130658248f46960d09827f6111b6
Juan David Varela Requejo <juan.varela01+globant@globant.com>
2025-09-04 09:49:45 -0500

    Se elimina carpeta SRC

---
+++ app/(dashboard)/[userId]/layout.tsx
@@ -0,0 +1,42 @@
+import { DashboardBreadcrumb } from "./components/dashboard-breadcrumb";
+import { DashboardSidebar } from "./components/dashboard-sidebar";
+import { Separator } from "../../../components/shared/separator/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "../../../components/ui/sidebar";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ userId: string }>;
+}) {
+  const { userId } = await params;
+
+  console.log({ userId });
+
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+
+            <DashboardBreadcrumb />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
