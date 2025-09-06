3f7fff392af1923c3ed3f19703b2dddb16ce3fe5
Juan David Varela Requejo <juan.varela01+globant@globant.com>
2025-09-06 10:45:17 -0500

    Modelos de Prisma implementados

--- app/(dashboard)/[userId]/(routes)/profile/page.tsx
+++ app/(dashboard)/[userId]/(routes)/profile/page.tsx
@@ -9,108 +9,19 @@ import { EmptyState } from "@/components/shared/empty-state/empty-state";
 import { maskValue } from "@/utils/mask-value";
 import { ProfileForm } from "./components/profile-form";
 
-import {
-  Doctor,
-  Medications,
-  Patient,
-  PatientMedications,
-  PatientRelevantConditions,
-  RelevantConditions,
-} from "@prisma/client";
+interface SettingsPageProps {
+  params: Promise<{ userId: string }>;
+}
 
-type PatientResponse = Patient & {
-  doctor: Doctor;
-  relevantConditions: (PatientRelevantConditions & {
-    relevantCondition: RelevantConditions;
-  })[];
-  medications: (PatientMedications & { medication: Medications })[];
-};
-
-const SettingsPage = () => {
-  const params = useParams();
-  const userId = params?.userId;
-
-  const [loading, setLoading] = useState<boolean>(true);
-  const [patient, setPatient] = useState<PatientResponse | null>(null);
-  const [medications, setMedications] = useState<Medications[]>([]);
-  const [relevantConditions, setRelevantConditions] = useState<
-    RelevantConditions[]
-  >([]);
-
-  useEffect(() => {
-    const fetchRelevantConditions = async () => {
-      try {
-        setLoading(true);
-
-        const data = await apiClient.get<RelevantConditions[]>(
-          "/relevant-conditions"
-        );
-
-        setRelevantConditions(data);
-      } catch (error) {
-        toast.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const fetchMedications = async () => {
-      try {
-        setLoading(true);
-
-        const data = await apiClient.get<Medications[]>("/medications");
-
-        setMedications(data);
-      } catch (error) {
-        toast.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    const fetchMedicalHistory = async () => {
-      try {
-        setLoading(true);
-
-        const data = await apiClient.get<PatientResponse>(`/users/${userId}`);
-
-        setPatient(data);
-      } catch (error) {
-        toast.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    if (userId) {
-      fetchMedicalHistory();
-      fetchRelevantConditions();
-      fetchMedications();
-    }
-  }, [userId]);
-
+const SettingsPage: React.FC<SettingsPageProps> = ({ params }) => {
   const initialData = {
-    name: patient?.name ?? "",
-    email: patient?.email ?? "",
-    birthdate: patient?.birthdate
-      ? typeof patient.birthdate === "string"
-        ? new Date(patient.birthdate)
-        : patient.birthdate
-      : new Date(),
-    gender: patient?.gender ?? "",
-    doctorAccessCode: maskValue(patient?.doctor?.accessCode ?? ""),
-    height: patient?.height ?? 0,
-    weight: patient?.weight ?? 0,
-    relevantConditions:
-      patient?.relevantConditions?.map((rc) => ({
-        id: rc.relevantCondition.id,
-        name: rc.relevantCondition.name,
-      })) ?? [],
-    medications:
-      patient?.medications?.map((m) => ({
-        id: m.medication.id,
-        name: m.medication.name,
-      })) ?? [],
+    relevantConditions: [],
+    medications: [],
+    name: "",
+    email: "",
+    birthdate: new Date(),
+    gender: "",
+    doctorAccessCode: "",
   };
 
   if (loading || Object.keys(patient ?? {}).length === 0) {
@@ -131,11 +42,7 @@ const SettingsPage = () => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProfileForm
-          initialData={initialData}
-          relevantConditions={relevantConditions}
-          medications={medications}
-        />
+        <ProfileForm initialData={initialData} />
       </div>
     </div>
   );
