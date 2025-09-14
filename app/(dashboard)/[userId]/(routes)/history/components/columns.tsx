80d34849d173e33aa7a1b829fb436df09896ae0e
jorgearmd <jorgearmd@gmail.com>
2025-09-13 20:26:11 -0500

    fix(columns.tsx): corregido bug en columna 'systolicPressure' que mostraba por error la presión diastólica

--- app/(dashboard)/[userId]/(routes)/history/components/columns.tsx
+++ app/(dashboard)/[userId]/(routes)/history/components/columns.tsx
@@ -38,7 +38,7 @@ export const columns: ColumnDef<MeasurementColumns>[] = [
           {row.original.systolicPressure}
         </span>
       ) : (
-        row.original.systolicPressure
+        row.original.diastolicPressure
       ),
   },
   {
