f86bb97a44584cf56793cab56fc96f204017a9c2
Juan David Varela Requejo <juan.varela01+globant@globant.com>
2025-09-15 10:25:00 -0500

    Se soluciona problema de datos pintados en la tabla

--- app/(dashboard)/[userId]/(routes)/history/components/columns.tsx
+++ app/(dashboard)/[userId]/(routes)/history/components/columns.tsx
@@ -60,7 +60,7 @@ export const columns: ColumnDef<MeasurementColumns>[] = [
           {row.original.diastolicPressure}
         </span>
       ) : (
-        row.original.systolicPressure
+        row.original.diastolicPressure
       ),
   },
   {
