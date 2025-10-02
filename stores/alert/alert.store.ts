1bc7d5355a7623126f2c7548ddc86de61fac1c38
Juan David Varela Requejo <juan.varela01+globant@globant.com>
2025-10-01 19:13:19 -0500

    Solucionado problema de persist para el cacheo de variables de alertas

--- stores/alert/alert.store.ts
+++ stores/alert/alert.store.ts
@@ -6,31 +6,38 @@ export interface AlertState {
   setToken: (token: string) => void;
   showHypertensionAlert: boolean;
   showHypotensionAlert: boolean;
-  notificationPermission: 'default' | 'granted' | 'denied';
+  notificationPermission: "default" | "granted" | "denied";
   setShowHypertensionAlert: (show: boolean) => void;
   setShowHypotensionAlert: (show: boolean) => void;
-  setNotificationPermission: (permission: 'default' | 'granted' | 'denied') => void; 
+  setNotificationPermission: (
+    permission: "default" | "granted" | "denied"
+  ) => void;
 }
 
 const storeApi: StateCreator<AlertState> = (set) => ({
   token: "",
   showHypertensionAlert: false,
   showHypotensionAlert: false,
-  notificationPermission: 'default', 
-  
+  notificationPermission: "default",
+
   setToken: (token: string) => set({ token }),
   setShowHypertensionAlert: (show: boolean) =>
     set({ showHypertensionAlert: show }),
   setShowHypotensionAlert: (show: boolean) =>
     set({ showHypotensionAlert: show }),
-  setNotificationPermission: (permission) => set({ notificationPermission: permission }), 
+  setNotificationPermission: (permission) =>
+    set({ notificationPermission: permission }),
 });
 
 export const useAlertStore = create<AlertState>()(
   devtools(
     persist(storeApi, {
       name: "alert-storage",
-      partialize: (state) => ({ token: state.token }),
+      partialize: (state) => ({
+        showHypertensionAlert: state.showHypertensionAlert,
+        showHypotensionAlert: state.showHypotensionAlert,
+        token: state.token,
+      }),
     })
   )
 );
